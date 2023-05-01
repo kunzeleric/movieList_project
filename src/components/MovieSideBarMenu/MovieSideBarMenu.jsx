155f1a9fd99c119bedb50a16efe2444ed1df21a5
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-05-01 20:37:32 -0300

    feat: Criado menu hamburger e alguns ajustes na responsividade (em andamento)

--- src/components/MovieSideBarMenu/MovieSideBarMenu.jsx
+++ src/components/MovieSideBarMenu/MovieSideBarMenu.jsx
@@ -1,44 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faStar,
   faShip,
   faBoxes,
   faFire,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
-import "./index.scss";
 import { Link } from "react-router-dom";
+import "./index.scss";
 
 const MovieSideBarMenu = () => {
-  return (
-    <div className='sidebar'>
-      <nav className="sidebar__nav">
-        <ul className="sidebar__nav-list">
-          <li className='active'>
-            <Link to={"/"} className='link'>
-              <FontAwesomeIcon icon={faShip} className='icon' />
-              Navigate
-            </Link>
-          </li>
-          <li>
-            <Link to='toprated' className='link'>
-              <FontAwesomeIcon icon={faStar} className='icon' />
-              Top Rated
-            </Link>
-          </li>
+  const [menuVisible, setMenuVisible] = useState(false);
+  
 
-          <li>
-            <Link to='categories' className='link'>
-              <FontAwesomeIcon icon={faBoxes} className='icon' />
-              Categories
+  function HideMenu() {
+    const sideBar = document.querySelector(".sidebar");
+    const pElements = Array.from(document.querySelectorAll('p'));
+
+    if (!menuVisible) {
+      sideBar.classList.toggle('resize');
+      
+      pElements.map((item) => {
+        item.classList.toggle('hidden');
+      })
+
+      setMenuVisible(true);
+    } else {
+      sideBar.classList.toggle('resize');
+      pElements.map((item) => {
+        item.classList.toggle('hidden');
+  
+      })
+      setMenuVisible(false);
+    }
+  }
+
+  return (
+    <div className="sidebar">
+      <nav className="sidebar__nav">
+        <FontAwesomeIcon icon={faBars} className="iconMenu" onClick={HideMenu}/>
+        <ul className="sidebar__nav-list">
+        <Link to={"/"} className="link">
+            <li className="sidebar__nav-list__item active">
+              <div className="sidebar__nav-list__item-wrapper">
+                <FontAwesomeIcon icon={faShip} className="icon"/>
+                <p>Navigate</p>
+              </div>
+            </li>
+          </Link>
+
+            <Link to="toprated" className="link">
+              <li className="sidebar__nav-list__item">
+                <div className="sidebar__nav-list__item-wrapper">
+                  <FontAwesomeIcon icon={faStar} className="icon" />
+                  <p>Top Rated</p>
+                </div>
+              </li>
             </Link>
-          </li>
-          <li>
-            <Link to='mostwatched' className='link'>
-              <FontAwesomeIcon icon={faFire} className='icon' />
-              Most Watched
+
+            <Link to="categories" className="link">
+              <li className="sidebar__nav-list__item">
+                <div className="sidebar__nav-list__item-wrapper">
+                  <FontAwesomeIcon icon={faBoxes} className="icon" />
+                  <p>Categories</p>
+                </div>
+              </li>
             </Link>
-          </li>
+
+            <Link to="mostwatched" className="link">
+              <li className="sidebar__nav-list__item">
+                <div className="sidebar__nav-list__item-wrapper">
+                  <FontAwesomeIcon icon={faFire} className="icon" />
+                  <p>Most Watched</p>
+                </div>
+              </li>
+            </Link>
+
         </ul>
       </nav>
     </div>
